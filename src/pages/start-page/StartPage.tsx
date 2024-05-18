ef63c5ee601b5f74b58f35c13810b3a03c4c78ea
AshkanZj <ashkanzojaji19@gmail.com>
2024-05-18 16:12:19 +0330

    feat: StartPage Logic

--- src/pages/start-page/StartPage.tsx
+++ src/pages/start-page/StartPage.tsx
@@ -1,14 +1,25 @@
 import Quiz from "../../assets/images/quiz.svg";
 import SpaceShip from "../../assets/images/space-ship.gif";
+import { useFormContext } from "../../context/FormContext";
+import { QuizActionTypesEnum } from "../../types/types";
 
 const StartPage = () => {
+  const { formDispatch }: any = useFormContext();
   return (
     <div className="md:w-[45%] w-4/5 h-4/5 bg-violet-500 rounded-2xl m-auto p-10 flex flex-col items-center justify-between">
       <img src={Quiz} alt="quiz" className="w-44" />
       <h3 className="text-white text-2xl font-bold">Welcome to the quiz app</h3>
       <div className="flex flex-col items-center gap-3">
         <h4 className="text-white text-lg font-bold">GET START</h4>
-        <div className=" bg-white rounded-full p-2 cursor-pointer">
+        <div
+          className=" bg-white rounded-full p-2 cursor-pointer"
+          onClick={() => {
+            formDispatch({
+              type: QuizActionTypesEnum.CHANGE_PAGE,
+              payload: { page: 1 },
+            });
+          }}
+        >
           <img src={SpaceShip} alt="space ship" />
         </div>
       </div>
